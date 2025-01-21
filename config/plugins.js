e97d5e34f44ac9024b6d7e44a7ef982ce351fdc2
Luis G. Villegas <lvillegas.ex@meatcode.cl>
2025-01-20 22:21:05 -0400

    feat: configure meilisearch from plugins config file

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,12 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
+  meilisearch: {
+    config: {
+      // Your meili host
+      host: env('MEILISEARCH_HOST', 'http://localhost:7700'),
+      // Your master key or private key
+      apiKey: env('MEILISEARCH_KEY', 'masterKey'),
+    },
+  },
   'users-permissions': {
     config: {
       register: {
