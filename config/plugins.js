c161cbb27e30dadb30d59f6968f776228ed8b603
Luis G. Villegas <lvillegas.ex@meatcode.cl>
2025-01-20 22:32:54 -0400

    feat: add logs

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,21 @@
-module.exports = ({ env }) => ({
-  meilisearch: {
-    config: {
-      // Your meili host
-      host: env('MEILISEARCH_HOST', 'http://localhost:7700'),
-      // Your master key or private key
-      apiKey: env('MEILISEARCH_KEY', 'masterKey'),
+module.exports = ({ env }) => {
+  console.log('MEILISEARCH_HOST:', env('MEILISEARCH_HOST'));
+  console.log('MEILISEARCH_KEY:', env('MEILISEARCH_KEY'));
+  return {
+    meilisearch: {
+      config: {
+        // Your meili host
+        host: env('MEILISEARCH_HOST', 'http://localhost:7700'),
+        // Your master key or private key
+        apiKey: env('MEILISEARCH_KEY', 'masterKey'),
+      },
     },
-  },
-  'users-permissions': {
-    config: {
-      register: {
-        allowedFields: ['firstName', 'lastName', 'phoneNumber', 'cedula', 'birthdate', 'role'],
-      }
+    'users-permissions': {
+      config: {
+        register: {
+          allowedFields: ['firstName', 'lastName', 'phoneNumber', 'cedula', 'birthdate', 'role'],
+        }
+      },
     },
-  },
-});
+  }
+};
