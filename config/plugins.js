978507a5a6f3c31354687176aa50d83933044476
Luis G. Villegas <lvillegas.ex@meatcode.cl>
2025-01-20 22:43:35 -0400

    fix remove meilisearch variables

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,5 @@
 module.exports = ({ env }) => {
-  console.log('MEILISEARCH_HOST:', env('MEILISEARCH_HOST'));
-  console.log('MEILISEARCH_KEY:', env('MEILISEARCH_KEY'));
   return {
-    meilisearch: {
-      config: {
-        // Your meili host
-        host: env('MEILISEARCH_HOST', 'http://localhost:7700'),
-        // Your master key or private key
-        apiKey: env('MEILISEARCH_KEY', 'masterKey'),
-      },
-    },
     'users-permissions': {
       config: {
         register: {
